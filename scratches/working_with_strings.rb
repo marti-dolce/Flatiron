e195c1b3364562f8f35a5d57a2e6944fbdea09b1
Martinique <me@martidolce.com>
2020-11-10 12:51:19 -0500

    Made some significant changes to this directory structure.

---
+++ scratches/working_with_strings.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+
